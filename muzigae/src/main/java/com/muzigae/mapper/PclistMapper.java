f9a0c7bc0cb005e5a762ef8c4b4f55bd59614a8a
soob0519 <sbeen0519@gmail.com>
2025-05-22 20:54:42 +0900

    조수빈 2025-05-22

--- muzigae/src/main/java/com/muzigae/mapper/PclistMapper.java
+++ muzigae/src/main/java/com/muzigae/mapper/PclistMapper.java
@@ -5,10 +5,26 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.muzigae.dto.DbuyDto;
+import com.muzigae.dto.DcustomerDto;
+import com.muzigae.dto.DpaymentDto;
+import com.muzigae.dto.DpurchaseDto;
 
 @Mapper
 public interface PclistMapper {
 
 	List<?> selectFullList(DbuyDto dto);
+
+	List<DpurchaseDto> selectPcDetail(int bseqid);
+
+	DbuyDto selectdbuyDetail(int bseqid);
+
+	DcustomerDto selectDcustomerDetail(String user_id);
+
+	DpaymentDto selectDpaymentDetail(int bseqid);
+	
+	List<?> selectBuyList(DbuyDto dto);
+
+	List<?> selectCancelList(DbuyDto dto);
+
 
 }
