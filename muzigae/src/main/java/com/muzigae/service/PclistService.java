3a45f564b589024fa5b9235917aab4c56b9ea918
soob0519 <sbeen0519@gmail.com>
2025-05-22 20:55:10 +0900

    조수빈 2025-05-22

--- muzigae/src/main/java/com/muzigae/service/PclistService.java
+++ muzigae/src/main/java/com/muzigae/service/PclistService.java
@@ -3,9 +3,40 @@ package com.muzigae.service;
 import java.util.List;
 
 import com.muzigae.dto.DbuyDto;
+import com.muzigae.dto.DcustomerDto;
+import com.muzigae.dto.DpaymentDto;
+import com.muzigae.dto.DpurchaseDto;
 
 public interface PclistService {
-
+	/**
+	 * 주문목록 전체 리스트
+	 */
 	List<?> selectFullList(DbuyDto dto) throws Exception;
-
+	/**
+	 * 구매 상세내역
+	 */
+	List<DpurchaseDto> selectPcDetail(int bseqid) throws Exception;
+	/**
+	 * 구매 상세내역 - 주문목록 관련
+	 */
+	DbuyDto selectdbuyDetail(int bseqid) throws Exception;
+	/**
+	 * 고객정보
+	 */
+	DcustomerDto selectDcustomerDetail(String user_id) throws Exception;
+	/**
+	 * 결제정보
+	 */
+	DpaymentDto selectDpaymentDetail(int bseqid) throws Exception;
+	/**
+	 * 주문목록 구매 리스트
+	 */
+	List<?> selectBuyList(DbuyDto dto) throws Exception;
+	/**
+	 * 주문목록 취소 리스트
+	 */
+	List<?> selectCancelList(DbuyDto dto) throws Exception;
+	
+	
+	
 }
--- muzigae/src/main/java/com/muzigae/service/PclistServiceImpl.java
+++ muzigae/src/main/java/com/muzigae/service/PclistServiceImpl.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.muzigae.dto.DbuyDto;
+import com.muzigae.dto.DcustomerDto;
+import com.muzigae.dto.DpaymentDto;
+import com.muzigae.dto.DpurchaseDto;
 import com.muzigae.mapper.PclistMapper;
 
 @Service
@@ -19,7 +22,34 @@ public class PclistServiceImpl implements PclistService {
 		return mapper.selectFullList(dto);
 	}
 
+	@Override
+	public List<DpurchaseDto> selectPcDetail(int bseqid) throws Exception {
+		return mapper.selectPcDetail(bseqid);
+	}
+
+	@Override
+	public DbuyDto selectdbuyDetail(int bseqid) throws Exception {
+		return mapper.selectdbuyDetail(bseqid);
+	}
+
+	@Override
+	public DcustomerDto selectDcustomerDetail(String user_id) throws Exception {
+		return mapper.selectDcustomerDetail(user_id);
+	}
+
+	@Override
+	public DpaymentDto selectDpaymentDetail(int bseqid) throws Exception {
+		return mapper.selectDpaymentDetail(bseqid);
+	}
+
+	@Override
+	public List<?> selectBuyList(DbuyDto dto) throws Exception {
+		return mapper.selectBuyList(dto);
+	}
 	
-	
-	
+	@Override
+	public List<?> selectCancelList(DbuyDto dto) throws Exception {
+		return mapper.selectCancelList(dto);
+	}
+
 }
